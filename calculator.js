7f4062e08110c48d4dd0619a054e7f5ec127197c
Dima Ryabov <dima.poltergeist.2015@yandex.ru>
2023-07-23 18:33:59 +0300

    накидал эскиз для калькулятора

---
+++ calculator.js
@@ -0,0 +1,5 @@
+const input1 = document.querySelector(".input1");
+const input2 = document.querySelector(".input2");
+
+const btnPlus = document.querySelector(".plus");
+const minusBtn = document.querySelector(".minus");
